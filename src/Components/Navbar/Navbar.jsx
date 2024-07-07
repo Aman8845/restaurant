d339868717f55e7f14b256a10d7e9caa24f906e0
Aman Verma <amanv9952@gmail.com>
2024-07-07 21:20:22 +0530

    second commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import Logo from "../../Assets/Logo.svg"
+import { BsCart2 } from 'react-icons/bs'
+import { HiOutlineBars3 } from 'react-icons/hi2'
+import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <div className="nav-logo-container">
+        <img src="" alt="" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Testimonial</a>
+        <a href="">Contact</a>
+        <a href="">
+          <BsCart2 className='navbar-cart-icon' />
+        </a>
+        <button className="primary-button">Bookings Now</button>
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 />
+      </div>
+      <Drawer>
+        <Box>
+          <List>
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon></ListItemIcon>
+                <ListItemText />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider/>
+        </Box>
+      </Drawer>
+    </nav>
   )
 }
 
